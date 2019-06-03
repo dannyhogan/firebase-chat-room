803726b4ef97f33bb70a1b1c94acf0b254f8aa9a
Dan Hogan <hogan.daniel@hotmail.com>
2019-06-03 15:29:49 -0700

    Rendered header component in App and AuthApp

--- src/auth/AuthApp.js
+++ src/auth/AuthApp.js
@@ -1,6 +1,18 @@
 import Component from '../Component.js';
+import Header from '../shared/Header.js';
 
 class AuthApp extends Component {
+
+    render() {
+        const dom = this.renderDOM();
+        const main = dom.querySelector('main');
+
+        const header = new Header();
+        dom.insertBefore(header.render(), main);
+
+        return dom;
+    }
+
 
     renderTemplate() {
         return /*html*/`
