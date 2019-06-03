3c49de12b4f93f8895422a4e2770e26c0e347bce
Dan Hogan <hogan.daniel@hotmail.com>
2019-06-03 15:26:21 -0700

    Added config to firebase.js

--- src/services/firebase.js
+++ src/services/firebase.js
@@ -1,4 +1,12 @@
-const config = {};
+const config = {
+    apiKey: 'AIzaSyDZq4IzXcNhxMEY4-zXaaPIb0p55tGB4nI',
+    authDomain: 'chat-room-dde6b.firebaseapp.com',
+    databaseURL: 'https://chat-room-dde6b.firebaseio.com',
+    projectId: 'chat-room-dde6b',
+    storageBucket: 'chat-room-dde6b.appspot.com',
+    messagingSenderId: '202328850697',
+    appId: '1:202328850697:web:99fef7a6f642e467'
+};
 
 firebase.initializeApp(config);
 
