8f8134e59202cde03edf9235af126a592d5b116f
Dan Hogan <hogan.daniel@hotmail.com>
2019-06-03 16:25:22 -0700

    Update profile upon sign in or sign out

--- src/shared/Header.js
+++ src/shared/Header.js
@@ -1,13 +1,17 @@
 import Component from '../Component.js';
 import Profile from './Profile.js';
+import { auth } from '../services/firebase.js';
 
 class Header extends Component {
 
     render() {
         const dom = this.renderDOM();
-
         const profile = new Profile();
         dom.appendChild(profile.render());
+
+        auth.onAuthStateChanged(user => {
+            profile.update({ user });
+        });
 
         return dom;
     }
