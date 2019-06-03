788c3e96f8a520683f61159818205ed5ddae85fb
Dan Hogan <hogan.daniel@hotmail.com>
2019-06-03 16:25:40 -0700

    Add event listener for sign out if user is signed in

--- src/shared/Profile.js
+++ src/shared/Profile.js
@@ -1,6 +1,21 @@
 import Component from '../Component.js';
+import { auth } from '../services/firebase.js';
 
 class Profile extends Component {
+
+    render() {
+        const dom = this.renderDOM();
+        const user = this.props.user;
+        
+        if(user) {
+            const button = dom.querySelector('button');
+            button.addEventListener('click', () => {
+                auth.signOut();
+            });
+        }
+
+        return dom;
+    }
 
     renderTemplate() {
         const user = this.props.user;
@@ -8,6 +23,7 @@ class Profile extends Component {
         if(!user) {
             return `<div></div>`;
         }
+
         const avatar = user.photoURL || '../../assets/default.png';
 
         return /*html*/`
