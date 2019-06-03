ddb18d13c27e0d2abcf073fb1ebb8386caa8db3f
Dan Hogan <hogan.daniel@hotmail.com>
2019-06-03 16:09:17 -0700

    Added authUI and created div for render

--- src/auth/AuthApp.js
+++ src/auth/AuthApp.js
@@ -1,5 +1,8 @@
+import { auth } from '../services/firebase.js';
 import Component from '../Component.js';
 import Header from '../shared/Header.js';
+
+const ui = new firebaseui.auth.AuthUI(auth);
 
 class AuthApp extends Component {
 
@@ -10,6 +13,15 @@ class AuthApp extends Component {
         const header = new Header();
         dom.insertBefore(header.render(), main);
 
+        ui.start('#firebaseui-auth-container', {
+            signInOptions: [
+                firebase.auth.EmailAuthProvider.PROVIDER_ID,
+                firebase.auth.GoogleAuthProvider.PROVIDER_ID
+            ],
+            signInSuccessUrl: './',
+            credentialHelper: firebaseui.auth.CredentialHelper.NONE
+        });
+
         return dom;
     }
 
@@ -17,7 +29,10 @@ class AuthApp extends Component {
     renderTemplate() {
         return /*html*/`
             <div>
-                <main></main>
+                <main>
+                    <p>Sign in to start chatting!</p>
+                    <div id="firebaseui-auth-container"></div>
+                </main>
             </div>
         `;
     }
