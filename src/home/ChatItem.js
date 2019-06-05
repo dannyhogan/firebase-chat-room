dd02fd62953c643f1ebd781a92392a9fa39d4cd7
Dan Hogan <hogan.daniel@hotmail.com>
2019-06-05 16:55:02 -0700

    Add callback to ChatItem component

--- src/home/ChatItem.js
+++ src/home/ChatItem.js
@@ -1,13 +1,28 @@
 import Component from '../Component.js';
+import { auth } from '../services/firebase.js';
 
 class ChatItem extends Component {
 
+    render() {
+        const item = this.renderDOM();  
+        const onRemove = this.props.onRemove;
+        const removeButton = item.querySelector('#remove');
+
+        removeButton.addEventListener('click', () => {
+            onRemove(this.props.chatroom);
+        });
+
+        return item;
+    }
+
     renderTemplate() {
         const chatroom = this.props.chatroom;
+
         return /*html*/`
-            <li>
-                <h2>${chatroom.name} Chatroom</h2>
+            <li class="chat-item">
+                <h2>${chatroom.name} Chatroom -</h2>
                 <a href="/chat.html?key=${chatroom.key}"><button>Join Room</button></a>
+                <button class="${auth.currentUser.uid === chatroom.owner ? '' : 'disabled'}" id="remove">Delete</button>
             </li>
         `;
     }
