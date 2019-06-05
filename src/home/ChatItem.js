972c26bb9bb798ba3b2d8ee065afc92786bfe4c5
Dan Hogan <hogan.daniel@hotmail.com>
2019-06-05 14:34:14 -0700

    Added key to button for redirect

--- src/home/ChatItem.js
+++ src/home/ChatItem.js
@@ -4,11 +4,10 @@ class ChatItem extends Component {
 
     renderTemplate() {
         const chatroom = this.props.chatroom;
-
         return /*html*/`
             <li>
                 <h2>${chatroom.name} Chatroom</h2>
-                <a href="/chat.html"><button>Join Room</button></a>
+                <a href="/chat.html?key=${chatroom.key}"><button>Join Room</button></a>
             </li>
         `;
     }
