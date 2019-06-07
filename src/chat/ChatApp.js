8acf29c33339aca8c9c5bf302271bb4a8c1f0c67
Dan Hogan <hogan.daniel@hotmail.com>
2019-06-07 13:58:21 -0700

    Imported DB references and query utility

--- src/chat/ChatApp.js
+++ src/chat/ChatApp.js
@@ -3,7 +3,8 @@ import Component from '../Component.js';
 import Header from '../shared/Header.js';
 import ChatList from '../shared/ChatList.js';
 import MessageList from './MessageList.js';
-import { chatroomsRef } from '../services/firebase.js';
+import { chatroomsRef, messagesRef } from '../services/firebase.js';
+import QUERY from '../QUERY.js';
 
 class ChatApp extends Component {
 
@@ -17,12 +18,19 @@ class ChatApp extends Component {
         const chatList = new ChatList({ chatrooms: [] });
         main.appendChild(chatList.render());
 
-        const messageList = new MessageList();
+        const query = QUERY.parse(window.location.search.slice(1));
+
+        const messageList = new MessageList({ key: query.key, messages: [] });
         main.appendChild(messageList.render());
 
         chatroomsRef.on('value', snapshot => {
             const chatrooms = snapshot.val() ? Object.values(snapshot.val()) : [];
             chatList.update({ chatrooms });
+        });
+
+        messagesRef.child(query.key).on('value', snapshot => {
+            const messages = Object.values(snapshot.val());
+            messageList.update({ messages });
         });
 
         return dom;
