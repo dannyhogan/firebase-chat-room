7d58c2c4148e4a9d1ed0a2bb57200ca32cb44664
Dan Hogan <hogan.daniel@hotmail.com>
2019-06-06 17:20:22 -0700

    Imported chat list and message list intp chatapp

--- src/chat/ChatApp.js
+++ src/chat/ChatApp.js
@@ -1,6 +1,9 @@
 import '../utils/check-auth.js';
 import Component from '../Component.js';
 import Header from '../shared/Header.js';
+import ChatList from '../shared/ChatList.js';
+import MessageList from './MessageList.js';
+import { chatroomsRef } from '../services/firebase.js';
 
 class ChatApp extends Component {
 
@@ -10,6 +13,17 @@ class ChatApp extends Component {
 
         const header = new Header();
         dom.insertBefore(header.render(), main);
+
+        const chatList = new ChatList({ chatrooms: [] });
+        main.appendChild(chatList.render());
+
+        const messageList = new MessageList();
+        main.appendChild(messageList.render());
+
+        chatroomsRef.on('value', snapshot => {
+            const chatrooms = snapshot.val() ? Object.values(snapshot.val()) : [];
+            chatList.update({ chatrooms });
+        });
 
         return dom;
     }
