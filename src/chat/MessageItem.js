2f2e3efa5ff11d2174b4208cb6064b8ab2e19a22
Dan Hogan <hogan.daniel@hotmail.com>
2019-06-07 13:58:53 -0700

    DDD mesage item component

--- src/chat/MessageItem.js
+++ src/chat/MessageItem.js
@@ -3,11 +3,18 @@ import Component from '../Component.js';
 class MessageItem extends Component {
 
     renderTemplate() {
+        const message = this.props.message;
+        const date = message.date.split('T')[0];
+
         return /*html*/`
             <li class="message-item">
-                <p class="user">Danny Hogan</p>
-                <p class="timestamp">@ 12-10-2019 8:15PM:</p>
-                <p class="message">What up party people</p>
+                <img class="user-image" src="${message.photoURL}">
+                <section class="message-details">
+                    <p class="user">${message.displayName}</p>
+                    <p class="timestamp">${date}</p>
+                </section>
+                <p class="colon">-</p>
+                <p class="message">${message.message}</p>
             </li>
         `;
     }
